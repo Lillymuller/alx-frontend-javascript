0e5a1f3d329e2ccdd82e9cac373071216a80728a
root <Lillymuller>
2024-06-26 11:46:10 +0000

    6-final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName)
-{
-  const v1 = signUpUser(firstName, lastName);
-  const v2 = uploadPhoto(fileName);
-
-  return Promise.allSettled([v1, v2]).then((value) => {
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
     const arr = [];
-    value.forEach((val) => {
-      if (val.status === 'fulfilled') {
-        arr.push({ status: val.status, value: val.value });
-      } else {
-        arr.push({ 
-          status: val.status,
-          value: `Error: ${val.reason.message}`,
-        });
-       }
-  });
+    values.forEach((val) => {
+      arr.push({
+        status: val.status,
+        value: val.status === 'fulfilled' ? val.value : `Error: ${val.reason.message}`,
+      });
+    });
     return arr;
   });
 }
